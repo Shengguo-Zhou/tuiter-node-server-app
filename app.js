f6a5f633657cc062dece4b147fe82df606e8def7
Shengguo Zhou <96205839+Shengguo-Zhou@users.noreply.github.com>
2023-04-02 17:13:08 -0700

    a9 all

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter);
+const DB_CONNECTION_STRING = 'mongodb+srv://giuseppi:supersecretpassword@cluster0.dsu2jvc.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_STRING =   DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
